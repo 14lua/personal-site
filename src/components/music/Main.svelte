8a6f58b735efcaf0e179a86904370c944cb4dc12
14lua <xhigoliluan022@gmail.com>
2025-01-08 20:50:12 +0100

    Added links

--- src/components/music/Main.svelte
+++ src/components/music/Main.svelte
@@ -6,20 +6,33 @@
         "I got into Wave and made this track in the style of classic Hardwave. This was actually made in one big rush of inspiration over only a few days.\nNot my proudest work, but I learnt a lot.",
         "This was my first ever track. I wanted to go with something simple so this is a very atmospheric and thoughtful track that was\nmeant to set the mood for my entire project."
     ];
+    let links = [
+        "",
+        "https://open.spotify.com/track/3pXeUJ5L6Tkqdnuj8mFaps?si=94f0f97e2074436b",
+        "https://open.spotify.com/track/6MP728l6GUAIVqqvoktAGd?si=15d2439ab2d34e8a",
+    ]
 </script>
 
 <main class="flex flex-col pl-24 pr-24 items-center justify-center">
     {#each names as name, i}
         <div class="grid grid-cols-2 w-[80%] h-[calc(100vh-75px)] items-center">
             <div class="flex pr-20">  
-                <img src="../images/{filenames[i]}" alt="" class="max-h-[50vh]"/>
+                <img src="../images/{filenames[i]}" alt="" class="max-h-[50vh] whiteGlow"/>
             </div>
-            <div class="flex flex-col pl-20 gap-12 justify-center">
+            <div class="flex flex-col pl-20 gap-8 justify-center">
                 <div>
                     <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">{name}</h2>
                 </div>
                 <div>
                     <p>{descriptions[i]}</p>
+                </div>
+                <div>
+                    <a
+                    href={links[i]}
+                    target="_blank"
+                    class="greenShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white hover:bg-gray-400 duration-200 text-gray-950"> 
+                        Listen on Spotify &rarr;
+                    </a>
                 </div>
             </div>
         </div>
